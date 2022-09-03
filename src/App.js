a811a0cf9983750a33dc7d546576e349ea133f15
Cesar Fierro <cesar.fierro@realpage.com>
2022-09-02 19:28:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-// El componente App es el padre de:
-// - Cabecera
-// - Listado
-// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
-// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
-// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+import React from 'react';
+import { useState } from 'react';
+import Cabecera from './components/Cabecera';
+import Listado from './components/Listado';
 
 function App() {
+  const [countItem, setCountItem] = useState(0);
+
+  const comprarItem = () => {
+    setCountItem(countItem + 1);
+  };
 
   return (
-    <div className="App">
-      <Cabecera />
-      <Listado />
+    <div className='App'>
+      <Cabecera cantidad={countItem} />
+      <Listado comprarItem={comprarItem} />
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,19 +1,28 @@
+import React from 'react';
+import { useState } from 'react';
 
-// El componente Item no tiene componentes hijos.
-// ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
-// MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
-// PROPS: Item recibe todos los campos que muestra en pantalla: nombre, descripcion, stock y el métodos heredados para su uso.
-// Maqueta de Item:
-//    h3
-//    p
-//    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
-//    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
+export default function Item({ comprarItem, nombre, descripcion, stock }) {
+  const [newStock, setNewStock] = useState(stock);
 
-export default function Item() {
+  const driverDeComprarItemAndCountItem = () => {
+    setNewStock(newStock - 1);
+    comprarItem();
+  };
 
   return (
     <div className='producto'>
-      {/* maquetar Item aquí */}
+      <h3>{nombre}</h3>
+      <p>{descripcion}</p>
+      {newStock > 0 ? (
+        <h5> En stock:{newStock}</h5>
+      ) : (
+        <h5>
+          En stock: <span>agotado</span>
+        </h5>
+      )}
+      <button disabled={!newStock} onClick={driverDeComprarItemAndCountItem}>
+        {newStock ? 'COMPRAR' : 'SIN STOCK'}
+      </button>
     </div>
-  )
+  );
 }
